8ca7b3e599f8340be119cf30e4f05e3df9fa1d88
kevinf <kevinfeng12345@gmail.com>
2024-09-01 15:03:41 -0400

    testing light mode only

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,8 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}", // Add this line to include any files in a src directory
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: false,
   theme: {
     extend: {
       colors: {
